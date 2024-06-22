32de77695673d47d13da6d1e009cf04565e17fb3
nikosanctioned <swiftpythondataai@icloud.com>
2024-06-22 15:06:11 +0500

    edit image

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import bg from "@/public/bg.png";
+import bg from "@/public/sasha.jpg";
 export default function Page() {
   return (
     <main className="mt-24">
